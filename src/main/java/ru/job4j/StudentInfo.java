25d0ea3deab2230b75d72b32c994a32f8c38d8ab
tatianafedorenko <0011tanya@mail.ru>
2022-10-17 13:09:13 +0300

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -3,7 +3,7 @@ package ru.job4j;
 public class StudentInfo {
 
     public static void main(String[] args) {
-        System.out.println("Tatiana Fedorenko") ;
-        System.out.println("15.04.1992") ;
+        System.out.println("Tatiana Fedorenko");
+        System.out.println("15.04.1992");
     }
 }
