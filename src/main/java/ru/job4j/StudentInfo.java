8fab50eefb91c687f1c39dde199f89c84fdf5579
tatianafedorenko <0011tanya@mail.ru>
2022-10-17 11:28:30 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+
+    public static void main(String[] args) {
+        System.out.println("Tatiana Fedorenko") ;
+        System.out.println("15.04.1992") ;
+    }
+}
