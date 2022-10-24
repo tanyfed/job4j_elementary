14bbe32a0de2c8b31d730dfc4cd527d5353db34d
tatianafedorenko <0011tanya@mail.ru>
2022-10-24 12:06:29 +0300

    Конвертер валюты [#41583]

---
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class SqArea {
+    public static double square(double p, double k) {
+        double h = p / (2 * (k + 1));
+        double i = h * k;
+        return i * h;
+    }
+
+    public static void main(String[] args) {
+        double result1 = SqArea.square(6, 2);
+        System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
+    }
+}
+
